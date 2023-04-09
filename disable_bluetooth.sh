aa4b195a0331bc41ffe8109b01446f3783744a20
onkar <onkarchendage@gmail.com>
2023-04-09 15:25:12 +0530

    Skip turning on Bluetooth if it was previously off before sleep

--- disable_bluetooth.sh
+++ disable_bluetooth.sh
@@ -1,10 +1,18 @@
 #!/bin/bash 
 
 file=~/.sleepscripts/recent.txt
+status=$(blueutil --power)
 
-blueutil --connected | grep -oE '[0-9A-Fa-f]{2}(-[0-9A-Fa-f]{2}){5}' > $file
+echo $status > $file
 
-blueutil --power 0
+if [ "$status" == "0" ]
+then 
+	exit 1
+else	
+	blueutil --connected | grep -oE '[0-9A-Fa-f]{2}(-[0-9A-Fa-f]{2}){5}' >> $file
+
+	blueutil --power 0
+fi
 
 # Uncomment to debug
 # echo "[$(date)] :attempting to disable bluetooth" >> ~/bluetooth.log
--- enable_bluetooth.sh
+++ enable_bluetooth.sh
@@ -1,14 +1,21 @@
 #!/bin/bash 
 
 file=~/.sleepscripts/recent.txt
+status=$(head -n 1 $file)
 
-blueutil --power 1
+if [ "$status" == "1" ]
+then
+	blueutil --power 1
 
-# loop through addresses and make an attempt to connect to each of them
-
-while read line; do 
-	blueutil --connect $line; 
-done < $file
+	# loop through addresses and make an attempt to connect to each of them
+	
+	i=1
+	while read line
+	do
+        	test $i -eq 1 && ((i=i+1)) && continue
+        	blueutil --connect $line
+	done < $file
+fi
 
 
 # Uncomment to debug
